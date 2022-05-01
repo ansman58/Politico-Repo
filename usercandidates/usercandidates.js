9995adc8f96b42c7101f7adddaea942c7fb05024
rnwonder <rnwonder@gmail.com>
2022-05-01 15:28:18 +0100

    init

--- usercandidates/usercandidates.js
+++
@@ -1,8 +0,0 @@
-const button = document.querySelectorAll('[type = "submit"]') 
-
-button.addEventListener('click',(event) => {
-    key = event.taget;
-    for(let i = 0; i < button.length; i++) {
-        alert(button[i]);
-    }
-})
