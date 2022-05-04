ba8493d911054faad75ef0f830d6f603bdf95eaf
ansman58 <65953362+ansman58@users.noreply.github.com>
2022-05-04 06:14:09 +0100

    m

--- admin page/admin.js
+++ admin page/admin.js
@@ -1,21 +1,58 @@
-const officeSubmitBtn = document.querySelector('.submit_for_office');
-const createOffice = document.querySelector('.create__political__office');
+// const officeSubmitBtn = document.querySelector('.submit_for_office');
+// const createOffice = document.querySelector('.create__political__office');
 
 
-const party = {
-    "id": 1,
-    "name": "APC",
-    "hqAddress": "Abuja",
-    "logoUrl": "igggb"
-}
+// const party = {
+//     "id": 1,
+//     "name": "APC",
+//     "hqAddress": "Abuja",
+//     "logoUrl": "igggb"
+// }
 
-function createParty(id, name, hqAddress,logoUrl){
-    for(let keys in party) {
-        console.log("heyy")
-    }
-    officeSubmitBtn.addEventListener('click', () => {
+// function createParty(id, name, hqAddress,logoUrl){
+//     for(let keys in party) {
+//         console.log("heyy")
+//     }
+//     officeSubmitBtn.addEventListener('click', () => {
         
-    })
-}
+//     })
+// }
 
-creatyParty();
+// creatyParty();
+const cardContainer = document.querySelector('.cards-container')
+const myIcons = document.querySelectorAll('icon-container')
+const editIcon = document.querySelector('.edit-icon')
+const deleteIcon = document.querySelector('.delete-icon')
+const deleteIt = document.querySelectorAll('to__be__deleted')
+
+// console.log(cardContainer.length)
+
+// myIcons.addEventListener('click', (event) => {
+//     const a = event.target
+//     console.log(a.length)
+//     if(a.classList.contains('icon-container')) {
+        
+//     }
+// })
+
+// Create a political party
+const createParty = document.querySelector('.create__party') 
+const createPartyModal = document.querySelector('.create__party__dailog')
+const createOffice = document.querySelector('.create__office__dialog')
+const createOfficeModal = document.querySelector('.create__office__dialog')
+const creaeOfficeBtn = document.querySelector('#create__party__btn')
+
+createParty.addEventListener('click', () => {
+   let openModal = createPartyModal.showModal();
+
+   if(creaeOfficeBtn) {
+        
+   }
+})
+
+createOffice.addEventListener('click', () => {
+    createOfficeModal.showModal();
+})
+
+
+
